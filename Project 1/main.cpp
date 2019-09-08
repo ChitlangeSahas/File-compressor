f1b6bfb5692ddefe4ad1544bd76a44e92571b1c3
Sahas Chitlange <chitlangesahas@gmail.com>
2019-09-08 15:28:04 -0700

    commit

--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <fstream>
+
+using namespace std;
+ifstream inFile;
+
+void	openFile()
+{
+	inFile.open("C:\\temp\\datafile.txt");
+}
+
+int main(int argc, char const *argv[])
+{
+	openFile();
+	return 0;
+}
