94af4019975736c2a06a0d9ad80b2145673de659
Sahas Chitlange <chitlangesahas@gmail.com>
2019-09-08 19:16:32 -0700

    added sample inputs and modified the main.cpp code

--- Project 1/main.cpp
+++ Project 1/main.cpp
@@ -1,28 +1,79 @@
 #include <stdio.h>
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <string.h>
+#include <sstream>
+#include <vector>
+std::string INPUT_FILEPATH = "./sample input/banksy-cnn.txt";
 
-std::ifstream inFile("input_file.txt");
-
-
-int main(int argc, char const *argv[])
+/*
+* split the strings into lines.
+*/
+std::vector<std::string> 	str_to_lines(std::string str, std::string delim)
 {
-	if(!inFile) 
+    std::vector<std::string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == std::string::npos) pos = str.length();
+        std::string token = str.substr(prev, pos-prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + delim.length();
+    }
+    while (pos < str.length() && prev < str.length());
+    return tokens;
+}
+
+/*
+* converts the incoming file and outputs the contents 
+* as a std::string 
+*/
+std::string 	file_to_str(std::string filePath)
+{
+	std::ifstream t(filePath);
+	std::stringstream buffer;
+	buffer << t.rdbuf();
+	return buffer.str();	
+} 
+
+std::vector<std::string>	cyclicShift(std::string line)
+{
+	char temp;
+	int length = line.length();
+	std::vector<std::string> shifted_lines;
+
+	for (int i = 0; i < length ; ++i)
 	{
-		std::cout << "Cannot open input file.\n";
-		return 1;
+		temp = line[0];
+		shifted_lines.push_back(line) ;
+		for (int i = 0; i < length; ++i)
+		{
+			if (i == (length-1)) 
+			{
+				line[i] = temp;
+			}
+			else
+			{
+				line[i] = line[i + 1];
+			}
+		}
+		// std::cout << line << std::endl;
 	}
+	return shifted_lines;
+}
 
-	char str[255];
-
-	// read line by line
-	while(inFile)
-	{
-		inFile.getline(str, 255);  // delim defaults to '\n'
-	    std::cout << str << std::endl;
-	}
-
-	inFile.close();
-
-	return 0;
+int 	main() {
+	std::string file_2_str = file_to_str(INPUT_FILEPATH); // file read as a string
+	 
+	std::vector<std::string>lines = str_to_lines(file_2_str, "\n");
+	std::vector<std::string> lines_CyclicShifted;
+	lines_CyclicShifted = cyclicShift(lines[9]);
+    
+    for (int i = 0; i < lines_CyclicShifted.size(); ++i)
+    {
+    	std::cout << lines_CyclicShifted[i] << std::endl;
+    }
+    
+    return 0;
 }
